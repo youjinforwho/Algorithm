9093761fbbecadae328bd2694a2ccc9e6610a1ae
yujpark <ox8764@naver.com>
2023-05-21 18:17:54 +0900

    TIL 20230521

--- 10989.py
+++ 10989.py
@@ -1,15 +1,11 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-li = []
+li = [0] * 10001
 for _ in range(n):
-    li.append(int(input()))
-freq = dict()
-for i in li:
-    if freq.get(i, 0) == 0:
-        freq[i] = 1
-    else:
-        freq[i] += 1
-for num, cnt in freq.items():
-    for _ in range(cnt):
-        print(num)
+    a = int(input())
+    li[a] += 1
+for i in range(10001):
+    if li[i] != 0:
+        for _ in range(li[i]):
+            print(i)
