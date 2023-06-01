5debeabfde21ccf75b884a9bc1b8078aae5785f4
youjinforwho <ox8764@naver.com>
2023-06-01 16:42:17 +0900

    whatthe

--- 10989.py
+++ 10989.py
@@ -2,6 +2,7 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 <<<<<<< HEAD
+<<<<<<< HEAD
 li = [0] * 10001
 for _ in range(n):
     a = int(input())
@@ -24,3 +25,13 @@ for num, cnt in freq.items():
     for _ in range(cnt):
         print(num)
 >>>>>>> a6a5d27 (TIL 20230521)
+=======
+li = [0] * 10001
+for _ in range(n):
+    a = int(input())
+    li[a] += 1
+for i in range(10001):
+    if li[i] != 0:
+        for _ in range(li[i]):
+            print(i)
+>>>>>>> edc0241 (TIL 20230521)
