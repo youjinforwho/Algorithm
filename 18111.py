7adedf739a34f9385b871e572cd1d113d8d12727
youjinforwho <ox8764@naver.com>
2023-06-03 21:04:49 +0900

    TIL 20230603

--- 18111.py
+++ 18111.py
@@ -1,18 +1,36 @@
+import sys
+input = sys.stdin.readline
 n, m, b = map(int, input().split())
 li = []
-mean = res = 0
 for i in range(n):
     arb = list(map(int, input().split()))
     for j in range(m):
         li.append(arb[j])
-        mean += arb[j]
-if (mean / (m * n) - mean // (m * n)) >= 0.5:
-    mean = int(mean / (m * n) + 0.5)
-else:
-    mean = mean // (m * n)
-for i in range(n * m):
-    if li[i] > mean:
-        res += (li[i] - mean) * 2
-    elif li[i] < mean:
-        res += (mean - li[i])
-print(res, mean)
+pl = 0
+pr = max(li)
+li.sort(reverse = True)
+min = int(2e9)
+while (pl <= pr):
+    flag = True
+    resource = b
+    spend = 0
+    mid = (pl + pr) // 2
+    for i in li:
+        if i > mid:
+            spend += 2 * (i - mid)
+            resource += i - mid
+        elif i < mid:
+            if resource >= mid - i:
+                resource -= mid - i
+                spend += mid - i
+            else:
+                flag = False
+                break
+    if flag == True:
+        if min >= spend:
+            answer = [spend, mid]
+            min = spend
+    else:
+        pr = mid - 1
+    print(mid)
+print(*answer)
