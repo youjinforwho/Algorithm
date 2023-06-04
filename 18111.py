d6ef5646e6b17a3ec7339fbca5342a42718cbfeb
youjinforwho <ox8764@naver.com>
2023-06-04 11:43:53 +0900

    TIL 20230604

--- 18111.py
+++ 18111.py
@@ -6,31 +6,25 @@ for i in range(n):
     arb = list(map(int, input().split()))
     for j in range(m):
         li.append(arb[j])
-pl = 0
-pr = max(li)
 li.sort(reverse = True)
 min = int(2e9)
-while (pl <= pr):
+for i in range(0, max(li) + 1):
     flag = True
     resource = b
     spend = 0
-    mid = (pl + pr) // 2
-    for i in li:
-        if i > mid:
-            spend += 2 * (i - mid)
-            resource += i - mid
-        elif i < mid:
-            if resource >= mid - i:
-                resource -= mid - i
-                spend += mid - i
+    for j in li:
+        if j > i:
+            spend += 2 * (j - i)
+            resource += j - i
+        elif j < i:
+            if resource >= i - j:
+                resource -= i - j
+                spend += i - j
             else:
                 flag = False
                 break
     if flag == True:
         if min >= spend:
-            answer = [spend, mid]
+            answer = [spend, i]
             min = spend
-    else:
-        pr = mid - 1
-    print(mid)
 print(*answer)
