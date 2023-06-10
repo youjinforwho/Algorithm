4e6fdacfaf5cc2ea72195fa29bd914804a169dd3
youjinforwho <ox8764@naver.com>
2023-06-10 19:12:54 +0900

    TIL 20230610

--- 1012.py
+++ 1012.py
@@ -1,23 +1,42 @@
-import sys
-read = sys.stdin.readline
+def DFS(n, m, check):
+    global cnt
+    flag = True
+    if n - 1 >= 0 and a[n - 1][m] == 1:
+        a[n][m] = 0
+        DFS(n - 1, m, check)
+        flag = False
+    if m - 1 >= 0 and a[n][m - 1] == 1:
+        a[n][m] = 0
+        DFS(n, m - 1, check)
+        flag = False
+    if n + 1 < N and a[n + 1][m] == 1:
+        a[n][m] = 0
+        DFS(n + 1, m, check)
+        flag = False
+    if m + 1 < M and a[n][m + 1] == 1:
+        a[n][m] = 0
+        DFS(n, m + 1, check)
+        flag = False
+    if flag == True and check == 0:
+        check = 1
+        a[n][m] = 0
 
-T = int(read())
+
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+T = int(input())
 for i in range(T):
     cnt = 0
-    M, N, K = map(int, read().split())
+    M, N, K = map(int, input().split())
     a = [[0] * M for _ in range(N)]
     for i in range(K):
-        x, y = map(int, read().split())
+        x, y = map(int, input().split())
         a[y][x] = 1
     for s in range(N):
         for k in range(M):
             if a[s][k] == 1:
-                if s - 1 >= 0 and k - 1 >= 0 and a[s - 1][k] == 0 and a[s][k - 1] == 0:
-                    cnt += 1
-                elif s - 1 >= 0 and k - 1 < 0 and a[s - 1][k] == 0:
-                    cnt += 1
-                elif s - 1 < 0 and k - 1 >= 0 and a[s][k - 1] == 0:
-                    cnt += 1
-                elif s - 1 < 0 and k - 1 < 0:
-                    cnt += 1
+                DFS(s, k, 0)
+                cnt += 1
     print(cnt)
