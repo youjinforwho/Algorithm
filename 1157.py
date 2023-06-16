488075a9889724658fbfd4e75e4a42834f57013f
youjinforwho <ox8764@naver.com>
2023-06-16 15:36:22 +0900

    TIL 20230616

---
+++ 1157.py
@@ -0,0 +1,12 @@
+word = input()
+dic = {}
+for i in word:
+    if dic.get(i.upper(), 0) == 0:
+        dic[i.upper()] = 1
+    else:
+        dic[i.upper()] += 1
+res = sorted(dic.items(), key = lambda x : x[1])
+if res[-1][1] == res[-2][1]:
+    print("?")
+else:
+    print(res[-1][0])
