fa533be772ba756e804a8acf66ebc973077ae594
yujpark <77055216+youjinforwho@users.noreply.github.com>
2024-04-08 18:48:06 +0900

    [Silver V] Title: 카드1, Time: 284 ms, Memory: 18676 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Queue<Integer> q = new LinkedList<>();
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.close();
+		int last = 0;
+		int temp;
+		
+		for (int i = 1; i <= N; i++) {
+			q.add(i);
+		}
+		while (!q.isEmpty()) {
+			last = q.element();
+			System.out.print(last + " ");
+			q.remove();
+			if (!q.isEmpty()) {
+				temp = q.element();
+				q.remove();
+				q.add(temp);
+			}
+		}
+	}
+}
