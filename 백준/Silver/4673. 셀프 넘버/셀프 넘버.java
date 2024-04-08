b657e20c1a2f208b55a89466731257062024c16d
yujpark <77055216+youjinforwho@users.noreply.github.com>
2024-04-08 17:53:37 +0900

    [Silver V] Title: 셀프 넘버, Time: 204 ms, Memory: 16452 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Set<Integer> set = new HashSet<>();
+		for (int i = 1; i <= 10000; i++) {
+			int num = i;
+			int temp = i;
+			while (temp != 0) {
+				num += (temp % 10);
+				temp /= 10;
+			}
+			set.add(num);
+		}
+		for (int i = 1; i <= 10000; i++) {
+			if (!set.contains(i)) {
+				System.out.println(i);
+			}
+		}
+	}
+}
