136e7695eeb7fc32e78078e2baac7dfa8f65ac6d
yujpark <77055216+youjinforwho@users.noreply.github.com>
2024-04-21 15:40:02 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14212 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,19 +1,20 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int L = sc.nextInt();
-		sc.nextLine();
-		String s = sc.nextLine();
-		long hash = 0;
-		for (int i = 0; i < L; i++) {
-			char A = s.charAt(i);
-			hash += ((int)A - 96) * Math.pow(31, i);
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int L = Integer.parseInt(br.readLine());
+		String S = br.readLine();
+		long result = 0;
+		long pow = 1;
+		for(int i = 0; i < L; i++) {
+			result += ((S.charAt(i) - 96) * pow);
+			pow = (pow * 31) % 1234567891;
 		}
-		System.out.println(hash % 1234567891);
-		sc.close();
+		System.out.println(result % 1234567891);
 	}
-}
 
+}
