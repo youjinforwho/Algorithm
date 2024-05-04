9358bf7ab0e92c03987b64766127a1967fbfc19a
yujpark <77055216+youjinforwho@users.noreply.github.com>
2024-05-04 10:43:36 +0900

    [Silver III] Title: 화살표 그리기, Time: 616 ms, Memory: 46208 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15975. 화살표 그리기/화살표 그리기.java
+++ 백준/Silver/15975. 화살표 그리기/화살표 그리기.java
@@ -1,56 +1,86 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
- 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
- 
-        int N = Integer.parseInt(br.readLine());
-        ArrayList<ArrayList<Integer>> a = new ArrayList<>();
-        for (int i = 0; i <= N; i++) {
-            a.add(new ArrayList<>());
+    public static void main(String[] args) throws IOException {
+        FastReader fr = new FastReader();
+        //입력받기
+        int N= fr.nextInt(); //점의 개수
+
+        ArrayList<int[]> Info = new ArrayList<>();
+        int colorSize[] = new int[N+1];
+        for(int i =0 ;i < N ;i++){
+            int x = fr.nextInt();
+            int color = fr.nextInt();
+            Info.add(new int[]{x,color});
+            colorSize[color]++;
         }
- 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            int point = Integer.parseInt(st.nextToken());
-            int color = Integer.parseInt(st.nextToken());
- 
-            a.get(color).add(point);
-        }
- 
-        for (int i = 0; i <= N; i++) {
-            Collections.sort(a.get(i));
-        }
- 
-        int ans = 0;
-        for (int i = 0; i <= N; i++) {
-            for (int j = 0; j < a.get(i).size(); j++) {
-                if (j == 0) {
-                    ans += a.get(i).get(j + 1) - a.get(i).get(j);
-                } else if (j == a.get(i).size() - 1) {
-                    ans += a.get(i).get(j) - a.get(i).get(j - 1);
-                } else {
-                    int t = a.get(i).get(j + 1) - a.get(i).get(j);
-                    int f = a.get(i).get(j) - a.get(i).get(j - 1);
- 
-                    ans += Math.min(t, f);
+        Collections.sort(Info, (a,b)->a[1] == b[1] ? a[0]-b[0] : a[1]-b[1]);//색깔별, x좌표순 별 정렬
+        long ans = 0;
+
+        for(int i = 0 ;i < Info.size() ;i++){
+            if (colorSize[Info.get(i)[1]] > 0) {
+                int left = Integer.MAX_VALUE;
+                int right = Integer.MAX_VALUE;
+                int ldx = i-1;
+                int rdx = i+1;
+                if(ldx >= 0 && Info.get(ldx)[1] == Info.get(i)[1]){
+                    left = Info.get(i)[0]-Info.get(ldx)[0];
                 }
+                if(rdx < Info.size() && Info.get(rdx)[1] == Info.get(i)[1]){
+                    right = Info.get(rdx)[0] - Info.get(i)[0];
+                }
+                int v = Math.min(left,right);
+                ans += v != Integer.MAX_VALUE ? v : 0;
             }
         }
- 
-        bw.write(ans + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(ans);
+
     }
- 
+
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
 }
