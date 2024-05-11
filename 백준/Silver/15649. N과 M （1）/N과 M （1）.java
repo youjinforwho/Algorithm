2602598d78db7b268987cad99075a47b260664b4
yujpark <77055216+youjinforwho@users.noreply.github.com>
2024-05-11 23:52:34 +0900

    [Silver III] Title: N과 M (1), Time: 564 ms, Memory: 41076 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+	public static void dfs(int depth, int M, int[] pick, boolean[] check) {
+		if (depth == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(pick[i] + " ");
+			}
+			sb.append("\n");
+			return ;
+		}
+
+		for (int i = 0; i < check.length; i++) {
+			if (!check[i]) {
+				check[i] = true;
+				pick[depth] = i + 1;
+				dfs(depth + 1, M, pick, check);
+				check[i] = false;
+			}
+		}
+	}
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		boolean[] check = new boolean[N];
+		int[] pick = new int[M];
+		dfs(0, M, pick, check);
+		System.out.println(sb);
+	}
+}
