ab8347ef7fd2b10c1bcb78dadb045c63140d5fa8
yujpark <77055216+youjinforwho@users.noreply.github.com>
2024-05-12 09:25:52 +0900

    [Silver III] Title: N과 M (4), Time: 288 ms, Memory: 21784 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int N;
+	public static void dfs(int start, int depth, int M, int[] pick) {
+		if (depth == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(pick[i] + " ");
+			}
+			sb.append("\n");
+			return ;
+		}
+
+		for (int i = start; i < N; i++) {
+			pick[depth] = i + 1;
+			dfs(i, depth + 1, M, pick);
+		}
+	}
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		int M = sc.nextInt();
+		int[] pick = new int[M];
+		dfs(0, 0, M, pick);
+		System.out.println(sb);
+	}
+}
