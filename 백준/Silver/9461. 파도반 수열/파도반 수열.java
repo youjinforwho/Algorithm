67cad46b13ea19582b8a787e91b70cedefe9399f
yujpark <77055216+youjinforwho@users.noreply.github.com>
2024-05-21 16:06:28 +0900

    [Silver III] Title: 파도반 수열, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		long[] dp = new long[101];
+		dp[0] = 0;
+		dp[1] = 1;
+		dp[2] = 1;
+		dp[3] = 1;
+		dp[4] = 2;
+		dp[5] = 2;
+		for (int i = 6; i <= 100; i++) {
+			dp[i] = dp[i - 1] + dp[i - 5];
+		}
+		while (T-- > 0) {
+			int N = Integer.parseInt(br.readLine());
+			System.out.println(dp[N]);
+		}
+	}
+}
