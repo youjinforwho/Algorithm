3e94c4e6091cb4e66e494b73d7700c25dd7c08c0
yujpark <77055216+youjinforwho@users.noreply.github.com>
2024-05-25 16:29:18 +0900

    [Silver I] Title: IOIOI, Time: 268 ms, Memory: 22008 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,31 +1,31 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int N = Integer.parseInt(br.readLine());
 		int M = Integer.parseInt(br.readLine());
-		String S = br.readLine();
-		
-		StringBuilder sb = new StringBuilder();
-		sb.append("I");
-		for (int i = 0; i < N; i++) {
-			sb.append("OI");
-		}
-		int cnt = 0;
-		String P = sb.toString();
-		for (int i = 0; i < S.length(); i++) {
-			char c = S.charAt(i);
-			if (c == P.charAt(0) && i + P.length() <= S.length()) {
-				boolean flag = true;
-				for (int j = 0; j < P.length(); j++) {
-					if (S.charAt(i + j) != P.charAt(j)) flag = false;
+		char s[] = br.readLine().toCharArray();
+
+		int result = 0;
+		int count = 0;
+
+		for(int i=1; i < M - 1; i++) {
+		if(s[i - 1] == 'I' && s[i] == 'O' && s[i + 1] == 'I') {
+				count++;
+
+				if(count == N) {
+					count--;
+					result++;
 				}
-				if (flag) cnt++;
+                i++;
+			}
+			else {
+				count = 0;
 			}
 		}
-		System.out.println(cnt);
-	}
-	
+		System.out.println(result);
+	} 
 } 
