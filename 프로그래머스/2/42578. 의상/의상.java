c3214f053617c62ac24f12510d5c85817829886b
yujpark <77055216+youjinforwho@users.noreply.github.com>
2024-09-05 17:48:44 +0900

    [level 2] Title: 의상, Time: 1.00 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,13 +1,13 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
+    
     public int solution(String[][] clothes) {
-        HashMap<String, Integer> map = new HashMap<>();
         int answer = 1;
+        HashMap<String, Integer> map = new HashMap<>();
         for (int i = 0; i < clothes.length; i++) {
-            if (map.get(clothes[i][1]) != null) {
-                map.put(clothes[i][1], map.get(clothes[i][1]) + 1);
-            } else map.put(clothes[i][1], 1);
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
         }
         for (String s : map.keySet()) {
             answer *= (map.get(s) + 1);
