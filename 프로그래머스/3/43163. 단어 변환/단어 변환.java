f7184bfe1cd20aa6212a5dd7bd81ead83051e0f8
yujpark <77055216+youjinforwho@users.noreply.github.com>
2024-10-04 15:48:48 +0900

    [level 3] Title: 단어 변환, Time: 0.57 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,8 +1,10 @@
 import java.util.*;
 class Solution {
-    static int answer;
+    static char[][] arr;
+    static int R, C, N;
     static boolean[] check;
-    static boolean calDiff(String s1, String s2) {
+    static int[] dist;
+    public static boolean wordCount(String s1, String s2) {
         int cnt = 0;
         for (int i = 0; i < s1.length(); i++) {
             if (s1.charAt(i) != s2.charAt(i)) cnt++;
@@ -10,24 +12,39 @@ class Solution {
         if (cnt == 1) return true;
         return false;
     }
-    static void dfs(int depth, int N, String begin, String target, String[] words) {
-        if (begin.equals(target)) {
-            if (answer > depth) answer = depth;
-            return ;
-        }
-        for (int i = 0; i < N; i++) {
-            if (!(check[i]) && calDiff(begin, words[i])) {
+    public static int bfs(String begin, String target, String[] words) {
+       Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < R; i++) {
+            if (wordCount(words[i], begin)) {
+                q.add(i);
                 check[i] = true;
-                dfs(depth + 1, N, words[i], target, words);
-                check[i] = false;
-            } 
+                dist[i] = 1;
+            }
         }
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            if (words[cur].equals(target)) {
+                return dist[cur];
+            }
+            for (int i = 0; i < N; i++) {
+                if (check[i]) continue;
+                if (wordCount(words[i], words[cur])) {
+                    q.add(i);
+                    check[i] = true;
+                    dist[i] = dist[cur] + 1;
+                }
+            }
+       }
+        return 0;
     }
     public int solution(String begin, String target, String[] words) {
-        check = new boolean[words.length];
-        answer = 2147483647;
-        dfs(0, words.length, begin, target, words);
-        if (answer == 2147483647) answer = 0;
-        return answer;
+        int answer = 0;
+        R = words.length;
+        C = words[0].length();
+        N = R;
+        dist = new int[N];
+        check = new boolean[N];
+
+        return bfs(begin, target, words);
     }
 }
